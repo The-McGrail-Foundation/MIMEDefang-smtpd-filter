50fb0777276c69bc92147120863d14c3b858fa06
Giovanni Bechis <giovanni@paclan.it>
2023-01-05 09:54:24 +0100

    new year

--- filter-mimedefang.pl
+++ filter-mimedefang.pl
@@ -2,7 +2,7 @@
 
 # MIT License
 #
-# Copyright (c) 2022 The McGrail Foundation
+# Copyright (c) 2022-2023 The McGrail Foundation
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
